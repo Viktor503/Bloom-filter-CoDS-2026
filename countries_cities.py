4fa71c5d2af692d3997c1f3cc009e9a98dd55806
benguimbis <benguimbis@live.fr>
2026-05-29 18:38:07 +0100

    Iteration correction and output folders
    I corrected the iteration error in the inner for loop.
    I have modified the code to export the results in csv in the output folder.

--- countries_cities.py
+++ countries_cities.py
@@ -11,15 +11,20 @@ size = list(range(1000, len(cities.index), 100))
 
 hash_num = list(range(1, 21))
 
-params = []
+df_list = []
 
 for m in size:    
     for k in hash_num:
         bf = BloomFilter(m, k)
         results = 0
-        for city_index in range(cities_string):
+        for city_index in range(len(cities_string)):
             if city_index < m:
                 bf.insert(cities_string[city_index])
                 continue
             results += bf.search(cities_string[city_index])
-        params.append((m, k, results/int(len(cities_string[m:]))))
+            temp_df = pd.DataFrame({"m" : m, "k" : k, "fpr": results/int(len(cities_string[m:]))})
+        df_list.append(temp_df)
+
+results_df = pd.concat(df_list, ignore_index=True)
+
+results_df.to_csv("output/data/cities_results.csv")
